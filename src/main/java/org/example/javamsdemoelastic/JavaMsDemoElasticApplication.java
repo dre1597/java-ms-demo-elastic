eb2a4c92e741ec58df6fa5657303f59042fa2087
André Pereira <andrepereira97@yahoo.com>
2025-05-04 16:37:08 -0300

    :tada: start project

---
+++ src/main/java/org/example/javamsdemoelastic/JavaMsDemoElasticApplication.java
@@ -0,0 +1,13 @@
+package org.example.javamsdemoelastic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMsDemoElasticApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JavaMsDemoElasticApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/example/javamsdemoelastic/JavaMsDemoElasticApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.javamsdemoelastic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMsDemoElasticApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
